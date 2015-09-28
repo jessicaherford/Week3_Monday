eefda45cc599b8816b038cf9eea6bacff627b575
calebatwood <calebatwood@Calebs-MacBook-Air.local>
2015-09-28 09:27:31 -0600

    Add index and main.js

---
+++ main.js
@@ -0,0 +1,55 @@
+// JavaScript Function Exercises
+
+// 1. Define a function called "divideByTwo". It should accept one parameter called "number".
+//    The function should divide the number by two and output the answer.
+
+// 2. Define a function called "greeting". It should accept two parameters, which will be names.
+//    The function should output the a greeting to both people.
+
+// 3. You use Amazon Subscribe & Save to have six cans of cashews automatically sent to you each month, but the price keeps changing.
+//    Write a function that takes the price per unit and calculates the total for you each month.
+
+// 4. Define a function called "quarter". It accepts one parameter called "number".
+//    The function will return a value which is one quarter of the number provided.
+//    Call the function inside of a conditional statement that prints one statement if the value is even or another statement if the value is odd.
+//    In pseudocode:
+//                  defining my function here(parameter)
+//                      returns a value that divides that parameter by 4
+//
+//                  if (calling the function with parameter divided by 2 is even)
+//                      print out a statement saying the number is even
+//                  else
+//                      print out a statement saying the number is odd
+
+// 5. Define functions called "area" and "perimeter". They should both accept two parameters and calculate the area and perimeter of a rectangle.
+
+// 6. Write a function called "sleepings", it should accept one parameter, an integer called "hours".
+//    Write a conditional statement inside of the function. If the number of hours is more than 8, print a statement to the console about getting enough rest.
+//    If the number of hours is less than 8, print a statement recommending the user get more shut eye.
+
+// 7. Prompt a user to enter a six digit number. Write a function that reverses the number.
+//    (Hint, you will have to turn the integer into a string before you can reverse it.)
+
+// 8. Within the fuction you wrote in #7, write a conditional statement that checks to ensure the user entered a six digit integer.
+//    If they have not entered a six digit integer, give them a message about being a failure.
+
+// 9. You've finally gotten around to counting the change in your piggy bank. Write a function that accepts four parameters (quarters, dimes, nickels, and pennies).
+//    The function should take each number of coins and multiply it times each coin's value.
+//    Finally, it should return the total amount of change you have in the following format: "$32.77"
+
+// 10. Develop a function that determines a person's age by prompting them for their birth year.
+
+// 11. Write a function that takes a year and reports whether or not it is a leap year.
+//     Remember, a leap year occurs:
+//        On every year that is evenly divisible by 4
+//        Except every year that is evenly divisible by 100
+//        Unless the year is also evenly divisible by 400
+//        For example, 1997 is not a leap year, but 1996 is.  1900 is not a leapyear, but 2000 is
+
+// 12. Develop a function that cleans up a phone number entered by a user.
+//     The rules are as follows:
+//        If the phone number is less than 10 digits assume that it is bad number
+//        If the phone number is 10 digits assume that it is good
+//        If the phone number is 11 digits and the first number is 1, trim the 1 and use the last 10 digits
+//        If the phone number is 11 digits and the first number is not 1, then it is a bad number
+//        If the phone number is more than 11 digits assume that it is a bad number
